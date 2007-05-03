3e5f7a23d976e43d238e870a6e708bd0e2f1b6eb
jahns <jahns@36be8a65-572f-0410-af5e-aa33ff25a6f6>
2007-05-03 09:16:09 +0000

    wth release 0.4.2

---
+++ wth_foundation/wth/tags/wth-0.4.2/config.h
@@ -0,0 +1,81 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define if you have <sys/wait.h> that is POSIX.1 compatible.  */
+#define HAVE_SYS_WAIT_H 1
+
+/* Define to `int' if <sys/types.h> doesn't define.  */
+/* #undef pid_t */
+
+/* Define as the return type of signal handlers (int or void).  */
+#define RETSIGTYPE void
+
+/* Define to `unsigned' if <sys/types.h> doesn't define.  */
+/* #undef size_t */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if your <sys/time.h> declares struct tm.  */
+/* #undef TM_IN_SYS_TIME */
+
+/* Enable GNU extensions */
+#define _GNU_SOURCE
+
+/* CPU_VENDOR_OS */
+#define CPU_VENDOR_OS "freebsd4.4"
+
+/* Define if you have the mktime function.  */
+#define HAVE_MKTIME 1
+
+/* Define if you have the select function.  */
+#define HAVE_SELECT 1
+
+/* Define if you have the socket function.  */
+#define HAVE_SOCKET 1
+
+/* Define if you have the strcspn function.  */
+#define HAVE_STRCSPN 1
+
+/* Define if you have the strdup function.  */
+#define HAVE_STRDUP 1
+
+/* Define if you have the strerror function.  */
+#define HAVE_STRERROR 1
+
+/* Define if you have the strspn function.  */
+#define HAVE_STRSPN 1
+
+/* Define if you have the <fcntl.h> header file.  */
+#define HAVE_FCNTL_H 1
+
+/* Define if you have the <strings.h> header file.  */
+#define HAVE_STRINGS_H 1
+
+/* Define if you have the <sys/ioctl.h> header file.  */
+#define HAVE_SYS_IOCTL_H 1
+
+/* Define if you have the <syslog.h> header file.  */
+#define HAVE_SYSLOG_H 1
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
+
+/* Define if you have the <xmlrpc.h> header file.  */
+#define HAVE_XMLRPC_H 1
+
+/* Define if you have the <xmlrpc_abyss.h> header file.  */
+#define HAVE_XMLRPC_ABYSS_H 1
+
+/* Define if you have the <xmlrpc_client.h> header file.  */
+#define HAVE_XMLRPC_CLIENT_H 1
+
+/* Name of package */
+#define PACKAGE "wth"
+
+/* Version number of package */
+#define VERSION "0.4.2"
+
---
+++ wth_foundation/wth/tags/wth-0.4.2/version.h
@@ -0,0 +1,2 @@
+/* wth version */
+char * wth_version = "experimental test release 0.4.2 - December 2001";
