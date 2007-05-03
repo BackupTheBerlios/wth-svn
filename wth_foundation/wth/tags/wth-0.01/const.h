2e13602bbbc833d77f40223b7c6a387c23b04bfa
jahns <jahns@36be8a65-572f-0410-af5e-aa33ff25a6f6>
2007-05-03 09:07:02 +0000

    wth release 0.01

---
+++ wth_foundation/wth/tags/wth-0.01/const.h
@@ -0,0 +1,16 @@
+/* const.h */
+
+#define SOH 0x01
+#define STX 0x02
+#define ETX 0x03
+#define EOT 0x04
+#define ENQ 0x05
+#define ACK 0x06
+#define DLE 0x10
+#define DC2 0x12
+#define DC3 0x13
+#define NAK 0x15
+
+#define FALSE 0
+#define TRUE 1
+
---
+++ wth_foundation/wth/tags/wth-0.01/global.h
@@ -0,0 +1,16 @@
+/* global.h */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <termios.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <sys/signal.h>
+#include <syslog.h>
+
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
