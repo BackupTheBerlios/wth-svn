484c7aaa3602508e6bfb976377dc08cda52f760f
vjahns <vjahns@36be8a65-572f-0410-af5e-aa33ff25a6f6>
2007-07-22 20:07:18 +0000

    dbmk with pcwsr

--- wth_foundation/wth/trunk/wthnewpd.c
+++ wth_foundation/wth/trunk/wthnewpd.c
@@ -29,21 +29,32 @@ main ( int argc, char **argv )
   tzset(); /* setting timezone */
 
   /* PCWSR thread */
-  printf("Creating PCWSR thread: ");
-  ret = pthread_create( &ptid, NULL, ploghandler, NULL);
-  printf ("ret : %d\n", ret);
+  if ( strncmp( pcwsrstation.config.device, "/dev/", 5) == 0) {
+    printf("Creating PCWSR thread:\n");
+    printf("\tpcwsrstation.config.device: %s\n", pcwsrstation.config.device);
+    ret = pthread_create( &ptid, NULL, ploghandler, NULL);
+    printf ("ret : %d\n", ret);
+  }
 
   /* WS2000 thread */
-  printf("Creating WS2000 thread: ");
-  ret = pthread_create( &wtid, NULL, wloghandler, NULL);
-  printf ("ret : %d\n", ret);
-
+  if ( strncmp( ws2000station.config.device, "/dev/", 5) == 0) {
+    printf("Creating WS2000 thread: ");
+    printf("ws2000station.config.device: %s\n", ws2000station.config.device);
+    ret = pthread_create( &wtid, NULL, wloghandler, NULL);
+    printf ("ret : %d\n", ret);
+  }
   /* handling interactive commands */
   //pthread_create( &ctid, NULL, cmdhandler, NULL);
 
   //pthread_join( &tid_cmdhandler, NULL);
-  pthread_join( wtid, NULL);
-  pthread_join( ptid, NULL);
+
+  if ( strncmp( pcwsrstation.config.device, "/dev/", 5) == 0) {
+    pthread_join( ptid, NULL);
+  }
+
+  if ( strncmp( ws2000station.config.device, "/dev/", 5) == 0) {
+    pthread_join( wtid, NULL);
+  }
 
   exit(0);
 }
