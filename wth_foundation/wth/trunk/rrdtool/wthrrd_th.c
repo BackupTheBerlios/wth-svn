58e29500e858b4736b33ab7975baee6d3026215e
vjahns <vjahns@36be8a65-572f-0410-af5e-aa33ff25a6f6>
2007-11-20 08:41:18 +0000

    rrdtool integration

--- wth_foundation/wth/trunk/rrdtool/wthrrd_th.c
+++ wth_foundation/wth/trunk/rrdtool/wthrrd_th.c
@@ -11,13 +11,17 @@
 
 
 int main ( int argc, char **argv) {
-  struct rrd_context *wctx;
+  //struct rrd_context *wctx;
   long *pdp_step;
+  int rargc;
   char *template = "Temp1:Humidity1:Rain:Windspeed:Winddir:Winddev:PressureI";
+  //char **rargv = "887457267:22.5:50.0:1000:30.0:135.0:22.5:978";
 
-  wctx = rrd_get_context();
-
-  rrd_update_r( "PressureI.rrd", template, argc, argv);
+  //wctx = rrd_get_context();
+  rargc = 1;
+     
+  //rrd_update_r( "PressureI.rrd", template, argc, argv);
+  rrd_update(argc, argv);
   if ( rrd_test_error()) {
     printf("RRD Error: %s\n", rrd_get_error());
     return(-1); 
@@ -28,4 +32,8 @@ int main ( int argc, char **argv) {
 /*
   compile command:
   gcc -L/usr/local/lib -I/usr/local/include -I/usr/include -lpthread -lrrd_th -o wthrrd_th wthrrd_th.c
+
+  sample usage:
+  ./wthrrd_th PressureI.rrd 1200000000:22.7:50.0:1000:30.0:234:22.5:967
+
 */
