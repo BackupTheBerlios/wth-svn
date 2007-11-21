771a3ae8d3a4e633bed496d5d724e27334ef291a
vjahns <vjahns@36be8a65-572f-0410-af5e-aa33ff25a6f6>
2007-11-21 21:22:57 +0000

    usage rrd_update_r works

--- wth_foundation/wth/trunk/rrdtool/wthrrd_th.c
+++ wth_foundation/wth/trunk/rrdtool/wthrrd_th.c
@@ -11,17 +11,20 @@
 
 
 int main ( int argc, char **argv) {
-  //struct rrd_context *wctx;
+  struct rrd_context *wctx;
   long *pdp_step;
   int rargc;
-  char *template = "Temp1:Humidity1:Rain:Windspeed:Winddir:Winddev:PressureI";
-  //char **rargv = "887457267:22.5:50.0:1000:30.0:135.0:22.5:978";
+  char *filename = "Humidity1.rrd";
+  char *template = "Humidity1";
 
-  //wctx = rrd_get_context();
-  rargc = 1;
-     
-  //rrd_update_r( "PressureI.rrd", template, argc, argv);
-  rrd_update(argc, argv);
+  wctx = rrd_get_context();
+  printf("wthrrd_th: argc: %d\n", argc);
+  printf("wthrrd_th: argv[0]: %s\n", argv[0]);
+  printf("wthrrd_th: argv[1]: %s\n", argv[1]);
+  printf("wthrrd_th: argv[2]: %s\n", argv[2]);
+
+  rrd_update_r( filename, template, argc-2, (const char **)(argv + 2));
+  //rrd_update(argc, argv);
   if ( rrd_test_error()) {
     printf("RRD Error: %s\n", rrd_get_error());
     return(-1); 
@@ -31,9 +34,18 @@ int main ( int argc, char **argv) {
 
 /*
   compile command:
-  gcc -L/usr/local/lib -I/usr/local/include -I/usr/include -lpthread -lrrd_th -o wthrrd_th wthrrd_th.c
+  gcc -L/usr/local/lib -I/usr/local/include -I/usr/include -lpthread \
+  -lrrd_th -o wthrrd_th wthrrd_th.c
 
   sample usage:
-  ./wthrrd_th PressureI.rrd 1200000000:22.7:50.0:1000:30.0:234:22.5:967
+  ./wthrrd_th Humidity1.rrd 973709210:50.0
+ 
+  create a sample rrd to work with:
+  rrdtool create Humidity1.rrd  --start 973709200 --step 300 \
+  DS:Humidity1:GAUGE:600:10:100 \
+  RRA:AVERAGE:0.5:1:600 \
+  RRA:AVERAGE:0.5:6:700 \
+  RRA:AVERAGE:0.5:24:775 \
+  RRA:AVERAGE:0.5:288:797
 
 */
