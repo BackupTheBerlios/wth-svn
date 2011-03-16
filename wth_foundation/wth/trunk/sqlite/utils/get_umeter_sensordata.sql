5e4367d9f9144bde18c9ec19b95281c09739bc54
vjahns <vjahns@36be8a65-572f-0410-af5e-aa33ff25a6f6>
2011-03-16 13:38:05 +0000

    Ultimeter 2100 sqlite database data select

---
+++ wth_foundation/wth/trunk/sqlite/utils/get_umeter_sensordata.sql
@@ -0,0 +1,19 @@
+--
+-- get_umeter_sensordata.sql
+--
+SELECT DISTINCT 
+  sensornames.sensor_name,parameternames.parameter_name,
+  sensorupdate.last_update, sensordata.meas_value,
+  parameternames.unit 
+FROM  
+  sensorupdate, sensordata,sensornames,sensorparameters,parameternames 
+WHERE sensorupdate.last_update = sensordata.dataset_date 
+AND 
+  sensorupdate.sensor_meas_no = sensordata.sensor_meas_no 
+AND 
+  sensornames.sensor_no = sensorparameters.sensor_no 
+AND 
+  sensorupdate.sensor_meas_no = sensorparameters.sensor_meas_no 
+AND 
+  parameternames.parameter_no = sensorparameters.parameter_no
+;
